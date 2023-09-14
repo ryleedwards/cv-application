f5869f47fd02c7b8c37a20f9a914c0adc9bf4eb4
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-14 15:51:36 -0500

    Add form fields w input and state tracking

--- src/components/General.jsx
+++ src/components/General.jsx
@@ -1,1 +1,55 @@
 import { useState } from 'react';
+import '../styles/General.css';
+
+export default function General() {
+  const [state, setState] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  });
+
+  const handleInputChange = (e) => {
+    const changedInput = e.target.name;
+    setState((previousState) => {
+      return { ...previousState, [changedInput]: e.target.value };
+    });
+  };
+
+  return (
+    <form>
+      <label>First Name: </label>
+      <input
+        name='firstName'
+        type='text'
+        className='firstName'
+        value={state.firstName}
+        onChange={handleInputChange}
+      />
+      <label>Last Name: </label>
+      <input
+        name='lastName'
+        type='text'
+        className='lastName'
+        value={state.lastName}
+        onChange={handleInputChange}
+      />
+      <label>Email: </label>
+      <input
+        name='email'
+        type='email'
+        className='email'
+        value={state.email}
+        onChange={handleInputChange}
+      />
+      <label>Phone: </label>
+      <input
+        name='phone'
+        type='tel'
+        className='phone'
+        value={state.phone}
+        onChange={handleInputChange}
+      />
+    </form>
+  );
+}
