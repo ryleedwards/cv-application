b02f2f1ded7542e06a8c8eafabb52caba92c5745
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-16 23:32:25 -0500

    Add constant for education history

---
+++ src/utils/educationList.js
@@ -0,0 +1,20 @@
+export const educationList = [
+  {
+    schoolName: 'Harvard',
+    location: 'Boston, MA',
+    startDate: '08/01/2018',
+    endDate: '05/01/2022',
+    degree: 'BS',
+    fieldOfStudy: 'Computer Science',
+    description: 'Undergraduate',
+  },
+  {
+    schoolName: 'Princeton',
+    location: 'Princeton, NJ',
+    startDate: '08/01/2022',
+    endDate: '05/01/2024',
+    degree: 'MS',
+    fieldOfStudy: 'Computer Science',
+    description: "Master's",
+  },
+];
