ff20b4b934df4d0827cdb9ee3d20d85daa107c23
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-21 09:46:29 -0500

    Add fields to state

--- src/components/Education.jsx
+++ src/components/Education.jsx
@@ -1,1 +1,21 @@
 import { useState } from 'react';
+import { educationList } from '../utils/educationList';
+
+export default function Education() {
+  const [educationItem, setEducationItem] = useState({
+    schoolName: '',
+    location: '',
+    startDate: '',
+    endDate: '',
+    degree: '',
+    fieldOfStudy: '',
+    description: '',
+  });
+
+  return (
+    <form>
+      <label>School Name</label>
+      <input name='schoolName' type='text' />
+    </form>
+  );
+}
