20a33c8115213fc12876fec4d87b810b90a2302e
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-21 13:25:14 -0500

    Add generalInfo component logic
    Lift state (generalInfo) out of General.jsx
    Lift handleGeneralInputChange() out of General.jsx
    Pass props to <General /> and <ResumeOutput />

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import General from './components/General';
+import Work from './components/Work';
 import Education from './components/Education';
 import ResumeOutput from './components/ResumeOutput';
 import './styles/styles.css';
+import { useState } from 'react';
 
 function App() {
+  const [generalInfo, setGeneralInfo] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+  });
+
+  const handleGeneralInputChange = (e) => {
+    const changedInput = e.target.name;
+    setGeneralInfo((previousGeneralInfo) => {
+      return { ...previousGeneralInfo, [changedInput]: e.target.value };
+    });
+  };
+
   return (
     <>
       <main>
         <div className='forms-container'>
-          <General />
+          <General
+            generalInfo={generalInfo}
+            handleGeneralInputChange={handleGeneralInputChange}
+          />
+          <Work />
           <Education />
         </div>
-        <ResumeOutput />
+        <ResumeOutput generalInfo={generalInfo} />
       </main>
     </>
   );
