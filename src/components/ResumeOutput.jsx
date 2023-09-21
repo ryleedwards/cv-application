38ec132da895bb86a613b0cf83b3bb21c9fdab51
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-21 12:55:19 -0500

    Add template jsx for work history

--- src/components/ResumeOutput.jsx
+++ src/components/ResumeOutput.jsx
@@ -16,20 +16,56 @@ export default function ResumeOutput() {
         <p className='work-history-title'>Work History</p>
         <ul>
           <li className='work-item'>
-            <p className='start-date'>2016-09</p>
-            <p className='end-date'>2018-04</p>
-            <p className='work-item-title'>Customer Service Specialist</p>
-            <p className='work-item-company'>Charles E. Cheese Entertainment</p>
-            <p className='work-item-location'>Austin, TX</p>
-            <ul className='work-item-responsibilities'>
-              <li className='responsibility-item'>
-                Supported customers in day-to-day activities.
-              </li>
-              <li className='responsibility-item'>
-                Implemented new team onboarding program, reducing training time
-                from four weeks to two.
-              </li>
-            </ul>
+            <div className='duration'>
+              <p className='start-date'>2016-09</p>
+              <p> - </p>
+              <p className='end-date'>2018-04</p>
+            </div>
+            <div className='work-details-container'>
+              <p className='work-item-title'>Customer Service Specialist</p>
+              <div className='work-name-location-container'>
+                <p className='work-item-company'>
+                  Charles E. Cheese Entertainment
+                </p>
+                <p>,</p>
+                <p className='work-item-location'>Austin, TX</p>
+              </div>
+              <ul className='work-item-responsibilities'>
+                <li className='responsibility-item'>
+                  Supported customers in day-to-day activities.
+                </li>
+                <li className='responsibility-item'>
+                  Implemented new team onboarding program, reducing training
+                  time from four weeks to two.
+                </li>
+              </ul>
+            </div>
+          </li>
+          <li className='work-item'>
+            <div className='duration'>
+              <p className='start-date'>2016-09</p>
+              <p> - </p>
+              <p className='end-date'>2018-04</p>
+            </div>
+            <div className='work-details-container'>
+              <p className='work-item-title'>Customer Service Specialist</p>
+              <div className='work-name-location-container'>
+                <p className='work-item-company'>
+                  Charles E. Cheese Entertainment
+                </p>
+                <p>,</p>
+                <p className='work-item-location'>Austin, TX</p>
+              </div>
+              <ul className='work-item-responsibilities'>
+                <li className='responsibility-item'>
+                  Supported customers in day-to-day activities.
+                </li>
+                <li className='responsibility-item'>
+                  Implemented new team onboarding program, reducing training
+                  time from four weeks to two.
+                </li>
+              </ul>
+            </div>
           </li>
         </ul>
       </div>
