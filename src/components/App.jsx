38576878b6605ef3f0195eccf5969ed034df00db
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-26 21:46:34 -0500

    Add work history logic
    add import for tempWorkList static job history
    add template fills for generalInfo
    add workList state
    add workItem state
    add logic for submitting new employment history
      item
    pass new props to <Work> and <ResumeOutput>

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,20 @@
 import General from './General';
 import Work from './Work';
-import Education from './Education';
 import ResumeOutput from './ResumeOutput';
 import '../styles/styles.css';
 import { useState } from 'react';
+import { tempWorkList } from '../utils/workList';
 
 function App() {
+  // generalInfo State Definition
   const [generalInfo, setGeneralInfo] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
+    firstName: 'First',
+    lastName: 'Last',
+    email: 'firstlast@gmail.com',
+    phone: '(512) 867-5309',
   });
 
+  // generalInfo Input Handler
   const handleGeneralInputChange = (e) => {
     const changedInput = e.target.name;
     setGeneralInfo((previousGeneralInfo) => {
@@ -20,20 +22,62 @@ function App() {
     });
   };
 
+  // workList State Definition
+  const [workList, setWorkList] = useState(
+    tempWorkList.map((workItem) => {
+      return {
+        id: workItem.id,
+        title: workItem.title,
+        employer: workItem.employer,
+        location: workItem.location,
+        startDate: workItem.startDate,
+        endDate: workItem.endDate,
+        description: workItem.description,
+      };
+    })
+  );
+
+  // workItem State Definition
+  const [workItem, setWorkItem] = useState({
+    id: '',
+    title: 'Worker',
+    employer: 'ABC Corp',
+    location: 'Austin, TX',
+    startDate: '2021-04-01',
+    endDate: '2021-05-01',
+    description: 'Did work things',
+  });
+
+  const handleWorkItemInputChange = (e) => {
+    const changedInput = e.target.name;
+    setWorkItem((previousWorkItem) => {
+      return { ...previousWorkItem, [changedInput]: e.target.value };
+    });
+  };
+
+  const handleEmploymentSubmitClick = (e) => {
+    e.preventDefault();
+    setWorkList((prevWorkList) => {
+      return [...prevWorkList, workItem];
+    });
+    console.log(workList);
+  };
+
   return (
-    <>
-      <main>
-        <div className='forms-container'>
-          <General
-            generalInfo={generalInfo}
-            handleGeneralInputChange={handleGeneralInputChange}
-          />
-          <Work />
-          <Education />
-        </div>
-        <ResumeOutput generalInfo={generalInfo} />
-      </main>
-    </>
+    <main>
+      <div>
+        <General
+          generalInfo={generalInfo}
+          handleGeneralInputChange={handleGeneralInputChange}
+        />
+        <Work
+          workItem={workItem}
+          handleWorkItemInputChange={handleWorkItemInputChange}
+          handleEmploymentSubmitClick={handleEmploymentSubmitClick}
+        />
+      </div>
+      <ResumeOutput generalInfo={generalInfo} workList={workList} />
+    </main>
   );
 }
 
