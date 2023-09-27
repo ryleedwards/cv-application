3f1c0bbd0e1529a3de53c4a10d8379571b9bd625
Ryle Edwards <ryle.edwards@gmail.com>
2023-09-26 21:54:39 -0500

    Add file for storing workList json

---
+++ src/utils/workList.js
@@ -0,0 +1,20 @@
+export const tempWorkList = [
+  {
+    id: 0,
+    startDate: '2016-09',
+    endDate: '2018-04',
+    title: 'Customer Service Specialist',
+    employer: 'Charles E. Cheese Entertainment',
+    location: 'Austin, TX',
+    description: 'Assisted in running day-to-day activities',
+  },
+  {
+    id: 1,
+    startDate: '2018-05',
+    endDate: '2021-03',
+    title: 'Night Guard',
+    employer: `Freddy Fazbear's Entertainment`,
+    location: 'Austin, TX',
+    description: 'Monitored night activities',
+  },
+];
